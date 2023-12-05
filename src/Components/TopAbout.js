b2882e01108b314b9cca6750d569ce264eb66d26
Caroline Eagen <Caroline.Eagen1@marist.edu>
2023-12-04 23:12:38 -0500

    Update

--- src/Components/TopAbout.js
+++ src/Components/TopAbout.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./style.css";
 import Map from './images/BikiniBottomMap.png';
-import Logos from './images/SocialLogos.png';
+import InstagramLogo from './images/Instagram.png';
+import FacebookLogo from './images/Facebook.png';
+import TwitterLogo from './images/Twitter.png';
 
 function TopAbout1() {
+
     return (
      
         <div style={{ display: 'flex', alignItems: 'center' , fontFamily: 'DM Sans, sans-serif', fontSize: 18, fontWeight: 'bold', backgroundColor: '#C8F0F4'}}>
@@ -18,7 +21,16 @@ function TopAbout1() {
                     <span style = {{ fontSize: 16, paddingLeft: '10px' }}>Phone: 718-224-5187</span><br />
                     <span style = {{ fontSize: 16, paddingLeft: '10px' }}>Email: inquiries@krustykrab.com</span><br />
                     <span style = {{ fontSize: 16, paddingLeft: '10px' }}>Follow Us: @KrustyKrab</span><br />
-                    <img src = {Logos} alt = "Logos" style={{maxWidth: '50%'}} /> 
+                    
+                    <a href="https://facebook.com/spongebob/" target="_blank" rel="noopener noreferrer">
+                        <img src={FacebookLogo} alt="Facebook Logo" style={{ maxWidth: '15%', cursor: 'pointer' }} />
+                    </a>
+                    <a href="https://twitter.com/spongebob" target="_blank" rel="noopener noreferrer">
+                        <img src={TwitterLogo} alt="Twitter Logo" style={{ maxWidth: '15%', cursor: 'pointer' }} />
+                    </a>
+                    <a href="https://instagram.com/spongebob" target="_blank" rel="noopener noreferrer">
+                        <img src={InstagramLogo} alt="Instagram Logo" style={{ maxWidth: '14%', cursor: 'pointer' }} />
+                    </a>
                 </div>
 
                 <div style={{ marginBottom: '20px', padding: '30px' , backgroundColor: '#DA1846', color: 'white', borderRadius: '8px', paddingTop: '10px', paddingBottom: '20px'}}>
